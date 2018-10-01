901de5d07c418ba5d1514737793aba2e0419629c
(Lim) Ming Wei <edward.mingwei@gmail.com>
2018-10-01 15:21:36 +0800

    added GraphicConstant

--- src/robot/RobotConstant.java
+++ src/robot/RobotConstant.java
@@ -98,8 +98,8 @@ public final class RobotConstant {
 	// Robot Default Configuration
 	public static final int DEFAULT_START_ROW = 18; // Changed to 1 based on ROBOT_SIZE
 	public static final int DEFAULT_START_COL = 1;
-	public static final int DEFAULT_GOAL_ROW = 18; 
-	public static final int DEFAULT_GOAL_COL = 1;
+	public static final int DEFAULT_GOAL_ROW = 1; 
+	public static final int DEFAULT_GOAL_COL = 18;
 	public static final DIRECTION DEFAULT_START_DIR = DIRECTION.UP;
     public static final DIRECTION DEFAULT_START_SP_DIR = DIRECTION.UP;
 	
