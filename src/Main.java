07dc815041f0b56257c2324c33eb5752345d16d8
Amartur Rahim <amarturrahim1795@gmail.com>
2018-10-02 11:18:50 +0800

    Make Sensor class for 'our'-friendly

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.concurrent.*;
 
 import utility.Comms;
@@ -10,28 +11,52 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException, InterruptedException {
 		
-		String mail;
+		// Messages Tracker
+		ArrayList<String> msgList = new ArrayList<String>();
+		int msgCount = 0;
+		String tmp;
 		
+		// Establish Connection	
 		Comms.openSocket();
 		
-		boolean test = Comms.sendMsg("TSDF");
-		System.out.println(test);
-		
+		// Receive Start Message
 		do {
-			mail = Comms.receiveMsg();
-		}while(mail==null);
-		System.out.println(mail);
+			tmp = Comms.receiveMsg();
+			System.out.println(tmp);
+		} while (tmp == null);
+		msgList.add(tmp); msgCount++;
 		
+		if (msgCount == 1) System.out.println("Initialising Start State");
 		
-		boolean fun = Comms.sendMsg(".");
-		System.out.println(fun);
-		
-		do {
-			mail = Comms.receiveMsg();
-		} while (mail==null);
-		System.out.println(mail);
-		
+		// 5 sec wait (as if to simulate start)
 		TimeUnit.MILLISECONDS.sleep(5000);
+		
+		// Send a message
+		boolean test = Comms.sendMsg("TSDF");
+		if(test) {
+			System.out.println("Successful Message Transmission");
+			do {
+				tmp = Comms.receiveMsg();
+				System.out.println(tmp);
+			} while (tmp == null);
+			msgList.add(tmp); msgCount++;
+		}
+		
+		// Send closing message and close socket if successful
+		boolean fun = Comms.sendMsg(".");
+		if(test) {
+			System.out.println("Successful Message Transmission");
+			do {
+				tmp = Comms.receiveMsg();
+				System.out.println(tmp);
+			} while (tmp == null);
+			msgList.add(tmp); msgCount++;
+			TimeUnit.MILLISECONDS.sleep(2000); // wait 2 seconds
+			Comms.closeSocket();
+		}
+		
+		// if failure, close after a specific time wait 
+		TimeUnit.MILLISECONDS.sleep(5000); // wait 5 seconds
 		Comms.closeSocket();
 		
 		
