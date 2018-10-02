df366d995cf086c671dd3275cb293b82e4f3d65e
Amartur Rahim <amarturrahim1795@gmail.com>
2018-10-02 12:00:35 +0800

    Changed 'Main.java'

--- src/Main.java
+++ src/Main.java
@@ -31,33 +31,13 @@ public class Main {
 		// 5 sec wait (as if to simulate start)
 		TimeUnit.MILLISECONDS.sleep(5000);
 		
-		// Send a message
-		boolean test = Comms.sendMsg("TSDF");
-		if(test) {
-			System.out.println("Successful Message Transmission");
+		while (true) {
 			do {
 				tmp = Comms.receiveMsg();
 				System.out.println(tmp);
 			} while (tmp == null);
 			msgList.add(tmp); msgCount++;
 		}
-		
-		// Send closing message and close socket if successful
-		boolean fun = Comms.sendMsg(".");
-		if(test) {
-			System.out.println("Successful Message Transmission");
-			do {
-				tmp = Comms.receiveMsg();
-				System.out.println(tmp);
-			} while (tmp == null);
-			msgList.add(tmp); msgCount++;
-			TimeUnit.MILLISECONDS.sleep(2000); // wait 2 seconds
-			Comms.closeSocket();
-		}
-		
-		// if failure, close after a specific time wait 
-		TimeUnit.MILLISECONDS.sleep(5000); // wait 5 seconds
-		Comms.closeSocket();
 		
 		
 	}
