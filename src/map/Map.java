5fb0f5a579e30702da7fd66655764b7c7ec7a154
(Lim) Ming Wei <edward.mingwei@gmail.com>
2018-10-10 16:14:23 +0800

    added idle sim, added Comms.open()

--- src/map/Map.java
+++ src/map/Map.java
@@ -346,7 +346,7 @@ public class Map extends JPanel{
 		 
         if (!_bMeasured) {
         	//TODO dummy debug
-        	System.out.println("Starting Simulator...");
+
 //            System.out.println("Map width: " + _mapWidth + ", Map height: " + _mapWidth);
 //            System.out.println(bot.getRobotRow()+","+bot.getRobotCol());
 //            System.out.println((bot.getRobotCol()+2) * GraphicConstant.TILE_SIZE - GraphicConstant.ROBOT_X_OFFSET - 5);
