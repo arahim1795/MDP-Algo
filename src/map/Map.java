8a52bb7a04c0480eec2ed78a0b4efbe8fba072d0
(Lim) Ming Wei <edward.mingwei@gmail.com>
2018-10-11 17:41:09 +0800

    minor fixes

--- src/map/Map.java
+++ src/map/Map.java
@@ -385,7 +385,8 @@ public class Map extends JPanel{
                 if (!isExploredTile(mapRow, mapCol)) {
                 	if(isMidPoint(mapRow,mapCol))
                 		gridColor = GraphicConstant.C_UNEXPLORED_MID;
-                	gridColor = GraphicConstant.C_UNEXPLORED;
+                	else
+                		gridColor = GraphicConstant.C_UNEXPLORED;
 
                 }
                 else {
