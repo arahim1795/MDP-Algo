031ec19baf8c01a6e8c8f9878f4ea2cf7b5c9f18
Amartur Rahim <amarturrahim1795@gmail.com>
2018-10-12 10:17:04 +0800

    Remove 1 function from Map.java

--- src/map/Map.java
+++ src/map/Map.java
@@ -201,7 +201,6 @@ public class Map extends JPanel {
 		}
 	}
 
-
 	// Setter(s)
 	/**
 	 * Sets Tile as an obstacles, adjacent Tile(s) are set as virtual walls, except for
@@ -486,10 +485,6 @@ public class Map extends JPanel {
 			}
 		}
 		this.repaint();
-	}
-
-	public Map getMap (Map map) {
-		return map;
 	}
 
 	private class ColorTile {
